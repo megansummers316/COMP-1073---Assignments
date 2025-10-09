6bffd36274c6d66b4b202f2183bd45431b6fc317
megansummers316 <megan.summers16@outlook.com>
2025-10-09 19:32:15 -0400

    more setup of variables/functions and adding p element to html for sentence

--- Assignment-1/seeNSay.js
+++ Assignment-1/seeNSay.js
@@ -7,6 +7,10 @@ let placeButton = document.querySelector('button#placeButton');
 //and surprise and playback button 
 let surprise = document.querySelector('button#surprise');
 let playback = document.querySelector('button#playback');
+//and initializer for sentence element
+let sentenceElement = document.querySelector('p#sentence');
+//declare variables for each column's selected value
+let person1, verb2, adjective3, thing4, place5;
 
 //add event listeners for each button
 personButton.addEventListener('click', person);
@@ -19,23 +23,23 @@ playback.addEventListener('click', playSentence)
 
 //functions for declaring which option user chooses
 function person() {
-
+    //7 options
 }
 function verb() {
-
+    //6 options
 }
 function adjective() {
-
+    //6 options
 }
 function thing() {
-
+    //7 options
 }
 function place() {
-
+    //6 options
 }
 function surpriseSentence() {
 
 }
 function playSentence() {
-    
+    sentenceElement.textContent = person1 + " " + verb2 + " " + adjective3 + " " + thing4 + " " + place5;
 }
