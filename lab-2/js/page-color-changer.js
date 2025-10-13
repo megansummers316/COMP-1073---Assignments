26394450da6aea23425b6fc0b33ed17d6d22a4ae
megansummers316 <megan.summers16@outlook.com>
2025-10-13 13:10:35 -0400

    comments

--- lab-2/js/page-color-changer.js
+++ lab-2/js/page-color-changer.js
@@ -5,7 +5,9 @@ const blueSlider = document.querySelector('#blue');
 const body = document.querySelector('body');
 
 //declare colours to make them global
-let red = 0, green = 0, blue = 0;
+let red = 0;
+let green = 0;
+let blue = 0;
 
 //add event listeners for the sliders (step 6)
 redSlider.addEventListener('change', changeRed);
@@ -13,14 +15,20 @@ greenSlider.addEventListener('change', changeGreen);
 blueSlider.addEventListener('change', changeBlue);
 //functions for event listeners (step 7)
 function changeRed() {
+    //get amount red is set to from slider
     red = Number(redSlider.value);
+    //reset background colour
     body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 function changeGreen() {
+    //get green amount from slider
     green = Number(greenSlider.value);
+    //reset background colour
     body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
 function changeBlue() {
+    //get blue amount from slider
     blue = Number(blueSlider.value);
+    //reset background colour
     body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
 }
