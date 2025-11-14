4da1e481ddf00bbc111be79ddb9b645235481687
megansummers316 <megan.summers16@outlook.com>
2025-11-14 15:46:24 -0500

    setting up class setup

---
+++ Assignment-2/smoothieMachine.js
@@ -0,0 +1,26 @@
+class Smoothie {
+    yogurt;
+    fruits;
+    nuts;
+    drizzle;
+    size;
+    constructor(yogurt, fruits, nuts, drizzle, size) {
+        this.yogurt = yogurt;
+        this.fruits = fruits;
+        this.nuts = nuts;
+        this.drizzle = drizzle;
+        this.size = size;
+    }
+    smoothieMaker() {
+        //yogurt options: vanilla, strawberry, blueberry, mixed berry, greek
+
+        //fruit options: Strawberries, raspberries, blueberries, blackberries, peaches, banana, kiwi, mango
+
+        //seed/nut options: chai seeds, hemp seeds, flax seeds, pumpkin seeds, almonds, walnuts, pecans, cashews, granola
+
+        //drizzle options: chocolate, honey, maple syurp, cinnamon
+
+        //size options: S - XL
+
+    }
+}
