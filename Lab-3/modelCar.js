08526478e4840557d395cd0e4d974250d0e957f7
megansummers316 <megan.summers16@outlook.com>
2025-11-16 18:54:34 -0500

    started styling

--- Lab-3/modelCar.js
+++ Lab-3/modelCar.js
@@ -26,7 +26,7 @@ class Truck {
     openableDoors;
     freewheeling;
     price = 20.99;
-    constructor(name, colour, scale, trim, packaging, openableDoors, freewheeling, price, stock){
+    constructor(name, colour, trim, openableDoors, freewheeling){
         this.name = name;
         this.colour = colour;
         this.trim = trim;
@@ -34,65 +34,69 @@ class Truck {
         this.freewheeling = freewheeling;
     }
     newTruck() {
-        const truck = document.createElement('img');
+        const truck = document.querySelector('img#truckImg');
         let truckImg;
         //this.name used for name throughout as it does not need modified from user input
         //determining chosen colour and trim and setting src accordingly
         if (this.trim === 'lowrider') {
             if (this.colour === 'white/orange') {
-                truckImg = 'img/white-orangeLowrider';
+                truckImg = 'img/white-orangeLowrider.jpg';
             }
             else if (this.colour === 'green') {
-                truckImg = 'img/greenLowrider';
+                truckImg = 'img/greenLowrider.png';
             }
             else if (this.colour === 'red') {
-                truckImg = 'img/greenLowrider';
+                truckImg = 'img/redLowrider.jpg';
             }
             else if (this.colour === 'blue/black') {
-                truckImg = 'img/blue-blackLowrider';
+                truckImg = 'img/blue-blackLowrider.png';
             }
         } else {
             if (this.colour === 'white/orange') {
-                truckImg = 'img/white-orangeStandard';
+                truckImg = 'img/white-orangeStandard.jpg';
             }
             else if (this.colour === 'green') {
-                truckImg = 'img/greenStandard';
+                truckImg = 'img/greenStandard.webp';
             }
             else if (this.colour === 'red') {
-                truckImg = 'img/greenStandard';
+                truckImg = 'img/redStandard.png';
             }
             else if (this.colour === 'blue/black') {
-                truckImg = 'img/blue-blackStandard';
+                truckImg = 'img/blue-blackStandard.png';
             }
         }
         truck.setAttribute('src', truckImg);
 
         //create and set a description
         let desc;
-        if (this.openableDoors === false && this.freewheeling === false) {
-            desc = `A ${this.colour} ${this.trim} Chevy C10 Fleetside Pickup Truck with a ${this.scale} scale in a ${this.packaging} are sold at \$${this.price}!`;
+        if (this.openableDoors === 'false' && this.freewheeling === 'false') {
+            desc = `${this.name} is a ${this.colour} ${this.trim} Chevy C10 Fleetside Pickup Truck with a ${this.scale} scale in a ${this.packaging}. Purchase them for \$${this.price}!`;
         }
-        else if (this.freewheeling === false) {
-            desc = `A ${this.colour} ${this.trim} Chevy C10 Fleetside Pickup Truck with openable doors has a ${this.scale} scale in a ${this.packaging} are sold at \$${this.price}!`;
+        else if (this.freewheeling === 'false') {
+            desc = `${this.name} is a ${this.colour} ${this.trim} Chevy C10 Fleetside Pickup Truck with openable doors has a ${this.scale} scale in a ${this.packaging}. Purchase them for \$${this.price}!`;
         }
-        else if (this.openableDoors === false) {
-            desc = `A ${this.colour} ${this.trim} 1966 Chevy C10 Fleetside Pickup Truck with freewheeling has a ${this.scale} scale in a ${this.packaging} are sold at \$${this.price}!`;
+        else if (this.openableDoors === 'false') {
+            desc = `${this.name} is a ${this.colour} ${this.trim} 1966 Chevy C10 Fleetside Pickup Truck with freewheeling has a ${this.scale} scale in a ${this.packaging}. Purchase them for \$${this.price}!`;
         }
-        const descEle = document.createElement('p');
+        else {
+            desc = `${this.name} is a ${this.colour} ${this.trim} 1966 Chevy C10 Fleetside Pickup Truck with openable doors and freewheeling as well as a ${this.scale} scale in a ${this.packaging}. Purchase them for \$${this.price}!`;
+        }
+        const descEle = document.querySelector('p#truckDesc');
         descEle.textContent = desc;
-
-        createSection.appendChild(truck);
-        createSection.appendChild(descEle);
     }
 }
 
 function createTruck() {
-    //dropdown values
+    //name and dropdown values
+    const nm = document.getElementById('tName').value;
     const clr = document.getElementById('colour').value;
     const trm = document.getElementById('trim').value;
+    const open = document.getElementById('openableDoors').value;
+    const tiresMove = document.getElementById('freewheeling').value;
 
     //create truck object
-    const truck = new Truck
+    const truck = new Truck(nm, clr, trm, open, tiresMove);
+    truck.newTruck();
 }
 
 showDetailsBtn.addEventListener('click', createTruck);
