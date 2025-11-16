cfc3d723b57551030d3e48317a7a3aa0698374fc
megansummers316 <megan.summers16@outlook.com>
2025-11-16 17:32:58 -0500

    creating description for truck

--- Lab-3/modelCar.js
+++ Lab-3/modelCar.js
@@ -13,7 +13,7 @@
     freewheeling: true
     price: 20.99 */
 
-
+const createSection = document.querySelector('div.create');
 const showDetailsBtn = document.querySelector('button.showDetails');
 
 class Truck {
@@ -40,9 +40,54 @@ class Truck {
         this.stock = stock;
     }
     newTruck() {
-        const img = document.createElement('img');
+        const truck = document.createElement('img');
         let truckImg;
         //this.name used for name throughout as it does not need modified from user input
-        
+        //determining chosen colour and trim and setting src accordingly
+        if (this.trim === 'lowrider') {
+            if (this.colour === 'white/orange') {
+                truckImg = 'orange lowrider source';//fix this
+            }
+            else if (this.colour === 'green') {
+                truckImg = 'green lowrider source';//fix this
+            }
+            else if (this.colour === 'red') {
+                truckImg = 'red lowrider source';//fix this
+            }
+            else if (this.colour === 'blue/black') {
+                truckImg = 'blue lowrider source';//fix this
+            }
+        } else {
+            if (this.colour === 'white/orange') {
+                truckImg = 'orange standard source';//fix this
+            }
+            else if (this.colour === 'green') {
+                truckImg = 'green standard source';//fix this
+            }
+            else if (this.colour === 'red') {
+                truckImg = 'red standard source';//fix this
+            }
+            else if (this.colour === 'blue/black') {
+                truckImg = 'blue standard source';//fix this
+            }
+        }
+        truck.setAttribute('src', truckImg);
+
+        //create and set a description
+        let desc;
+        if (this.openableDoors === false && this.freewheeling === false) {
+            desc = `A ${this.colour} ${this.trim} Chevy C10 Fleetside Pickup Truck with a ${this.scale} scale in a ${this.packaging} are sold at \$${this.price}!`;
+        }
+        else if (this.freewheeling === false) {
+            desc = `A ${this.colour} ${this.trim} Chevy C10 Fleetside Pickup Truck with openable doors has a ${this.scale} scale in a ${this.packaging} are sold at \$${this.price}!`;
+        }
+        else if (this.openableDoors === false) {
+            desc = `A ${this.colour} ${this.trim} Chevy C10 Fleetside Pickup Truck with freewheeling has a ${this.scale} scale in a ${this.packaging} are sold at \$${this.price}!`;
+        }
+        const descEle = document.createElement('p');
+        descEle.textContent = desc;
+
+        createSection.appendChild(truck);
+        createSection.appendChild(descEle);
     }
 }
