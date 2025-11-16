69604a1bae3c814584f8ea2c8a043090382c741a
megansummers316 <megan.summers16@outlook.com>
2025-11-16 17:03:15 -0500

    truck properties

---
+++ Lab-3/modelCar.js
@@ -0,0 +1,41 @@
+/* make: Chevrolet
+    model: C10 Fleetside Pickup Truck
+
+    name: user made
+    colour: white/orange
+    year: 1966
+    scale: 1/24
+    trim: Lowrider
+    packaging: window box
+    vehicleType: Pickup Truck
+    manufacturer: Motor Max
+    openableDoors: true
+    freewheeling: true
+    price: 20.99 */
+
+class Truck {
+    name;
+    colour;
+    year;
+    scale;
+    trim; // this means the lowrider or standard style
+    packaging;
+    vehicleType;
+    manufacturer;
+    openableDoors;
+    freewheeling;
+    price;
+    constructor(name, colour, year, scale, trim, packaging, vehicleType, manufacturer, openableDoors, freewheeling, price){
+        this.name = name;
+        this.colour = colour;
+        this.year = year;
+        this.scale = scale;
+        this.trim = trim;
+        this.packaging = packaging;
+        this.vehicleType = vehicleType;
+        this.manufacturer = manufacturer;
+        this.openableDoors = openableDoors;
+        this.freewheeling = freewheeling;
+        this.price = price;
+    }
+}
