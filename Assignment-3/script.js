76b85e2bbce634339cd9e8aed4da7941ca803323
megansummers316 <megan.summers16@outlook.com>
2025-11-30 21:05:30 -0500

    url and key declaration

---
+++ Assignment-3/script.js
@@ -0,0 +1,6 @@
+//url for NASA APOD API 
+const baseURL = 'https://api.nasa.gov/planetary/apod';
+
+//api key received in email from "Get Started" on webpage
+const key = 'o5G6k206tjCz8Pz5Voc1X9e2JCTbdAh9ZwMEKcwe';
+
